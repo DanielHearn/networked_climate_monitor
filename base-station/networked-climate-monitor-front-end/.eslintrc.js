8a1a48f77ba4b03cd28f3c65909b000dfd50db0a
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-12-16 20:49:37 +0000

    Add vue scaffolding

---
+++ base-station/networked-climate-monitor-front-end/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', '@vue/standard'],
+  rules: {
+    semi: [2, 'never'],
+    quotes: [1, 'single'],
+    'space-before-function-paren': ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-indent': 'warning',
+    'vue/html-quotes': 'warning',
+    'vue/no-multi-spaces': 'warning',
+    'vue/prop-name-casing': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/v-bind-style': 'error',
+    'vue/v-on-style': 'error',
+    'vue/attributes-order': 'error',
+    'vue/no-confusing-v-for-v-if': 'error',
+    'vue/no-v-html': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+    'vue/eqeqeq': 'error'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
