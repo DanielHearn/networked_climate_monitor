2c4aea363226932f12256b879733c7b21d5b1e61
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-12-25 09:57:49 +0000

    Fix settings data type

--- base-station/web-server/run.py
+++ base-station/web-server/run.py
@@ -41,7 +41,7 @@ class UserModel(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    settings = db.Column(db.text, nullable=False)
+    settings = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return '<UserModel %r>' % self.email
