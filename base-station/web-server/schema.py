c493394b4bde62891d4cae1276eaa85428e3636c
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-12-30 23:12:43 +0000

    Move schema to import file

---
+++ base-station/web-server/schema.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema, fields
+
+## SCHEMA
+
+class UserSchema(Schema):
+    email = fields.Email(required=True, validate=validate.Length(min=0, max=120))
+    password = fields.Str(required=True, validate=validate.Length(min=8, max=40))
+    settings = fields.Str(dump_only=True, required=True)
+
+
+class SensorSchema(Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=0, max=40))
+    sensor_id = fields.Integer(required=True)
+    user_id = fields.Integer(required=True)
+
+
+class ClimateDataSchema(Schema):
+    battery_voltage = fields.Float(required=True)
+    date = fields.DateTime(required=True)
+    climate_data = fields.List(fields.Dict(
+        value=fields.Integer(required=True),
+        type=fields.Str(required=True, validate=validate.Length(min=0, max=100)),
+        unit=fields.Str(required=True, validate=validate.Length(min=0, max=20))
+    ))
