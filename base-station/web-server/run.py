675005f5c1637ceacf1fea27607307dd9e4e7568
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-01-01 16:10:33 +0000

    Increase password size

--- base-station/web-server/run.py
+++ base-station/web-server/run.py
@@ -75,7 +75,7 @@ def unauthorized_loader_callback(msg):
 class UserModel(db.Model, SerializerMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     settings = db.Column(db.Text, nullable=False)
 
