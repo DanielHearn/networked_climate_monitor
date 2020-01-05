8b1979e3cd732e66860e4ebf825d35f45bbf6749
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-01-05 22:37:27 +0000

    Add dynamic titles to routes

--- base-station/front-end/src/router/index.js
+++ base-station/front-end/src/router/index.js
@@ -4,44 +4,84 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
+const titleEnd = ' - Climate Monitor'
+
 const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    meta: {
+      title: () => {
+        return `Home${titleEnd}`
+      },
+    },
   },
   {
     path: "/login",
     name: "login",
     component: () =>
-      import(/* webpackChunkName: "login" */ "../views/Login.vue")
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+    meta: {
+      title: () => {
+        return `Login${titleEnd}`
+      },
+    },
   },
   {
     path: "/register",
     name: "register",
     component: () =>
-      import(/* webpackChunkName: "register" */ "../views/Register.vue")
+      import(/* webpackChunkName: "register" */ "../views/Register.vue"),
+    meta: {
+      title: () => {
+        return `Register${titleEnd}`
+      },
+    },
   },
   {
     path: "/reset-password",
     name: "resetpassword",
     component: () =>
-      import(/* webpackChunkName: "resetpassword" */ "../views/ResetPassword.vue")
+      import(/* webpackChunkName: "resetpassword" */ "../views/ResetPassword.vue"),
+    meta: {
+      title: () => {
+        return `Reset Password${titleEnd}`
+      },
+    },
   },
   {
     path: "/dashboard",
     name: "dashboard",
     component: () =>
-      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue")
+      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue"),
+    meta: {
+      title: () => {
+        return `Dashboard${titleEnd}`
+      },
+    },
   },
   {
     path: "/settings",
     name: "settings",
     component: () =>
-      import(/* webpackChunkName: "settings" */ "../views/Settings.vue")
+      import(/* webpackChunkName: "settings" */ "../views/Settings.vue"),
+    meta: {
+      title: () => {
+        return `Settings${titleEnd}`
+      },
+    },
   },
-  { path: '*',     component: () =>
-  import(/* webpackChunkName: "notfound" */ "../views/NotFound.vue") }
+  {
+    path: '*',
+    component: () =>
+    import(/* webpackChunkName: "notfound" */ "../views/NotFound.vue"),
+    meta: {
+      title: () => {
+        return `404 Not Found${titleEnd}`
+      },
+    }
+  }
 ];
 
 const router = new VueRouter({
@@ -51,3 +91,9 @@ const router = new VueRouter({
 });
 
 export default router;
+
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    document.title = to.meta.title(to)
+  })
+})
