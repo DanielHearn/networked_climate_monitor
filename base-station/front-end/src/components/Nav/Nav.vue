0249bfe8f63f0f9a37fafe42a0738186854249bc
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-01-27 16:36:33 +0000

    Lint laptop changes

--- base-station/front-end/src/components/Nav/Nav.vue
+++ base-station/front-end/src/components/Nav/Nav.vue
@@ -12,34 +12,39 @@
         :isIcon="true"
       />
     </div>
-    <div class="nav__links" >
+    <div class="nav__links">
       <template v-if="!$store.state.mobile">
-      <router-link to="/" class="link">Home</router-link>
-      <router-link to="/login" v-if="!$store.state.user.logged_in" class="link"
-        >Login</router-link
-      >
-      <router-link
-        to="/register"
-        v-if="!$store.state.user.logged_in"
-        class="link"
-        >Register</router-link
-      >
-      <router-link
-        to="/dashboard"
-        v-if="$store.state.user.logged_in"
-        class="link"
-        >Dashboard</router-link
-      >
-      <router-link
-        to="/settings"
-        v-if="$store.state.user.logged_in"
-        class="link"
-        >Settings</router-link
-      >
+        <router-link to="/" class="link">Home</router-link>
+        <router-link
+          to="/login"
+          v-if="!$store.state.user.logged_in"
+          class="link"
+          >Login</router-link
+        >
+        <router-link
+          to="/register"
+          v-if="!$store.state.user.logged_in"
+          class="link"
+          >Register</router-link
+        >
+        <router-link
+          to="/dashboard"
+          v-if="$store.state.user.logged_in"
+          class="link"
+          >Dashboard</router-link
+        >
+        <router-link
+          to="/settings"
+          v-if="$store.state.user.logged_in"
+          class="link"
+          >Settings</router-link
+        >
       </template>
     </div>
     <div class="nav__side nav__right">
-      <logout-button v-if="$store.state.user.logged_in && !$store.state.mobile"/>
+      <logout-button
+        v-if="$store.state.user.logged_in && !$store.state.mobile"
+      />
       <v-button
         v-if="$store.state.mobile"
         @click.native="toggleMenu"
--- base-station/front-end/src/components/RegisterForm/RegisterForm.vue
+++ base-station/front-end/src/components/RegisterForm/RegisterForm.vue
@@ -1,50 +1,46 @@
 <template>
- <form id="register" class="form" @submit="checkRegister" method="post">
-          <div class="input-box">
-            <p class="text">Email</p>
-            <input
-              type="email"
-              class="input--text"
-              name="email"
-              v-model="email"
-              placeholder="Email"
-              tabindex="0"
-            />
-          </div>
-          <div class="input-box">
-            <p class="text">Password</p>
-            <input
-              type="password"
-              class="input--text"
-              name="password"
-              v-model="password"
-              placeholder="Password"
-              tabindex="0"
-            />
-          </div>
-          <div class="input-box">
-            <p class="text">Confirm Password</p>
-            <input
-              type="password"
-              class="input--text"
-              name="confirmPassword"
-              v-model="confirmPassword"
-              placeholder="Confirm password"
-              tabindex="0"
-            />
-          </div>
-          <error-list
-            v-if="errors.length"
-            title="Errors:"
-            v-bind:errors="errors"
-          />
-          <v-button
-            :hierachyLevel="'primary'"
-            :text="'Register'"
-            type="submit"
-            tabindex="0"
-          />
-        </form>
+  <form id="register" class="form" @submit="checkRegister" method="post">
+    <div class="input-box">
+      <p class="text">Email</p>
+      <input
+        type="email"
+        class="input--text"
+        name="email"
+        v-model="email"
+        placeholder="Email"
+        tabindex="0"
+      />
+    </div>
+    <div class="input-box">
+      <p class="text">Password</p>
+      <input
+        type="password"
+        class="input--text"
+        name="password"
+        v-model="password"
+        placeholder="Password"
+        tabindex="0"
+      />
+    </div>
+    <div class="input-box">
+      <p class="text">Confirm Password</p>
+      <input
+        type="password"
+        class="input--text"
+        name="confirmPassword"
+        v-model="confirmPassword"
+        placeholder="Confirm password"
+        tabindex="0"
+      />
+    </div>
+    <error-list v-if="errors.length" title="Errors:" v-bind:errors="errors" />
+    <v-button
+      :hierachyLevel="'primary'"
+      :text="'Register'"
+      type="submit"
+      tabindex="0"
+    />
+  </form>
 </template>
 
 <script src="./RegisterForm.js"></script>
