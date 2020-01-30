c8ef384a87bac82ea7e4cb20f5a2a6fe945c89d1
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-01-30 18:45:06 +0000

    Add API documentation

--- base-station/web-server/helpers.py
+++ base-station/web-server/helpers.py
@@ -3,6 +3,19 @@ import string
 
 
 def get_unit_from_type(unit_type):
+    """
+    Converts the sensor data type into the unit of measurement for that type
+
+    Parameters
+    ----------
+    type : string
+        String describing the sensor data type
+
+    Returns
+    -------
+    String
+        Unit of measurement for that sensor data type
+    """
     units = {
         'Humidity': '%',
         'Temperature': 'c',
@@ -12,6 +25,14 @@ def get_unit_from_type(unit_type):
 
 
 def create_settings():
+    """
+    Creates a default settings dictionary
+
+    Returns
+    -------
+    Dictionary
+        Dictionary representing all user settings
+    """
     settings = {
         "temperature_unit": "c"
     }
@@ -19,6 +40,14 @@ def create_settings():
 
 
 def generate_reset_key():
+    """
+    Generates a password reset token for use in the password reset page
+
+    Returns
+    -------
+    String
+        Password reset token
+    """
     length = 20
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
--- base-station/web-server/schema.py
+++ base-station/web-server/schema.py
@@ -1,28 +1,47 @@
 from marshmallow import Schema, fields, validate
 
+
 ## SCHEMA
 
 class UserSchema(Schema):
+    """
+    Schema representing a user
+    """
     email = fields.Email(required=True, validate=validate.Length(min=0, max=120))
     password = fields.Str(required=True, validate=validate.Length(min=8, max=40))
     settings = fields.Str(dump_only=True, required=True)
 
+
 class SensorSchema(Schema):
+    """
+    Schema representing a sensor
+    """
     name = fields.Str(required=True, validate=validate.Length(min=0, max=40))
     sensor_id = fields.Integer(required=True)
     user_id = fields.Integer(required=True)
 
+
 class SensorDataSchema(Schema):
+    """
+    Schema representing the data of an individual sensor reading
+    """
     value = fields.Float(required=True)
     type = fields.Str(required=True, validate=validate.Length(min=0, max=100))
     unit = fields.Str(required=True, validate=validate.Length(min=0, max=20))
 
+
 class ClimateDataSchema(Schema):
+    """
+    Schema representing the data of an individual climate data reading
+    """
     battery_voltage = fields.Float(required=True)
     date = fields.DateTime(required=True)
     climate_data = fields.List(fields.Nested(SensorDataSchema), required=True)
 
 
 class ChangePasswordSchema(Schema):
+    """
+    Schema representing the post data of the change password endpoint
+    """
     reset_token = fields.Str(required=True, validate=validate.Length(equal=40))
     password = fields.Str(required=True, validate=validate.Length(min=8, max=40))
