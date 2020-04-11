cca3866a785712c6f859c2037bb0a9ad2a64e2ee
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-04-12 00:22:00 +0100

    Improve wifi manager

--- base-station/web-server/wifi_manager.js
+++ base-station/web-server/wifi_manager.js
@@ -13,114 +13,98 @@ const mobileWifi = {
 
 const apiRoot = 'http://0.0.0.0/api/'
 const apiKey = 'xgLxTX7Nkem5qc9jllg2'
-const searchInterval = 30000
+const searchInterval = 120000
 let settings_retrieved = false
 
 function searchNetworks() {
-  wifi.scan(function(err, networks) {
+  // List the current wifi connections
+  wifi.getCurrentConnections(function(err, currentConnections) {
     if (err) {
       console.log(err);
-    } else {
-      //console.log(networks)
-      const stored_network = networks.filter((network) => network.ssid === storedWifi.ssid)[0]
-      const mobile_network = networks.filter((network) => network.ssid === mobileWifi.ssid)[0]
-      if(stored_network) {
-        console.log('Found stored network')
-      }
-      if(mobile_network) {
-        console.log('Found mobile network')
-      }
-
-      // List the current wifi connections
-      wifi.getCurrentConnections(function(err, currentConnections) {
+    }
+    var connected_ssid = null
+    if(currentConnections && currentConnections.length) {
+        connected_ssid = currentConnections[0].ssid
+    }
+    
+    if (connected_ssid) {
+      wifi.scan(function(err, networks) {
         if (err) {
           console.log(err);
-        }
-        var connected_ssid = null
-        if(currentConnections && currentConnections.length) {
-            connected_ssid = currentConnections[0].ssid
-        }
-        console.log(connected_ssid)
-        if (stored_network && storedWifi.ssid.length && connected_ssid !== storedWifi.ssid) {
-          console.log('Connecting to user\'s network')
-          const connectionData = {
-            ssid: storedWifi.ssid
+        } else {
+          const storedNetwork = networks.filter((network) => network.ssid === storedWifi.ssid)[0]
+          const mobileNetwork = networks.filter((network) => network.ssid === mobileWifi.ssid)[0]
+          if(storedNetwork) {
+            console.log('Found stored network')
           }
-          if (storedWifi.password) {
-            connectionData.password = storedWifi.password
+          if(mobileNetwork) {
+            console.log('Found mobile network')
           }
-          console.log(connectionData)
-          wifi.connect(connectionData, function(err) {
-            if (err) {
-              console.log(err);
-              if(mobile_network) {
-                wifi.connect({ ssid: mobileWifi.ssid, password: mobileWifi.password })
-                .then(function() {
-                  console.log('Connected')
-                })
-                .catch(function(error) {
-                  console.log(error)
-                });
-              }
-            } else {
-              console.log("Connected to user's network");
-            }
-          });
-        } else if(mobile_network && connected_ssid !== storedWifi.ssid && connected_ssid !== mobileWifi.ssid ) {
-          console.log('Connecting to mobile network')
-          wifi.connect({ ssid: mobileWifi.ssid, password: mobileWifi.password })
-              .then(function() {
-                console.log('Connected')
-              })
-              .catch(function(error) {
-                console.log(error)
-                if(stored_network) {
-                  const connectionData = {
-                    ssid: storedWifi.ssid
-                  }
-                  if (storedWifi.password) {
-                    connectionData.password = storedWifi.password
-                  }
-                  console.log(connectionData)
-                  wifi.connect(connectionData, function(err) {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      console.log("Connected to user's network");
-                    }
-                  });
-                }
-              });
+
+          if (storedNetwork && storedWifi.ssid.length && connected_ssid !== storedWifi.ssid) {
+            connectToUserNetwork(storedNetwork, mobileNetwork)
+          } else if(mobileNetwork && connected_ssid !== storedWifi.ssid && connected_ssid !== mobileWifi.ssid ) {
+            connectToMobileNetwork(storedNetwork, mobileNetwork)
+          }
         }
       });
     }
   });
-  setTimeout(searchNetworks, searchInterval)
 }
 
-async function setupSettingsReloading() {
-    setInterval(async ()=> {
-      const url = `${apiRoot}base-station-settings?api_key=${apiKey}`
-      try {
-        const response = await fetch(url)
-        const data = await response.json()
-        if (data && data.status && data.settings) {
-          const settings = JSON.parse(data.settings.replace(/'/g, '"'))
-          if (settings.wifi) {
-            storedWifi.ssid = settings.wifi.ssid
-            storedWifi.password = settings.wifi.password
-          }
-          console.log(storedWifi)
-        } else {
-          console.log('Bad response for settings retrieval')
-        }
-      } catch(e) {
-        console.log(e)
+function connectToUserNetwork(mobileNetwork) {
+  console.log('Connecting to user\'s network')
+  const connectionData = {
+    ssid: storedWifi.ssid
+  }
+  if (storedWifi.password) {
+    connectionData.password = storedWifi.password
+  }
+  wifi.connect(connectionData, function(err) {
+    if (err) {
+      console.log(err);
+      if(mobileNetwork) {
+        wifi.connect({ ssid: mobileWifi.ssid, password: mobileWifi.password })
+        .then(function() {
+          console.log('Connected')
+        })
+        .catch(function(error) {
+          console.log(error)
+        });
       }
-    }, 60000)
+    } else {
+      console.log("Connected to user's network");
+    }
+  });
 }
 
-async function loadInitialSettings() {
+function connectToMobileNetwork(storedNetwork) {
+  console.log('Connecting to mobile network')
+  wifi.connect({ ssid: mobileWifi.ssid, password: mobileWifi.password })
+  .then(function() {
+    console.log('Connected')
+  })
+  .catch(function(error) {
+    console.log(error)
+    if(storedNetwork) {
+      const connectionData = {
+        ssid: storedWifi.ssid
+      }
+      if (storedWifi.password) {
+        connectionData.password = storedWifi.password
+      }
+      wifi.connect(connectionData, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Connected to user's network");
+        }
+      });
+    }
+  });
+}
+
+async function loadSettings() {
   const url = `${apiRoot}base-station-settings?api_key=${apiKey}`
   try {
     const response = await fetch(url)
@@ -133,7 +117,6 @@ async function loadInitialSettings() {
         settings_retrieved = true
         console.log(storedWifi)
         searchNetworks()
-        setupSettingsReloading()
       }
     } else {
       console.log('Bad response for settings retrieval')
@@ -147,12 +130,10 @@ function init() {
   wifi.init({
     iface: 'wlan0'
   });
-  loadInitialSettings()
+  loadSettings()
   setInterval(() => {
-    if(!settings_retrieved) {
-      loadInitialSettings()
-    }
-  }, 60000)
+    loadSettings()
+  }, searchInterval)
 }
 
 init()
